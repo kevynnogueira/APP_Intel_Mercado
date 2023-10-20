c6e7e94a1b715616e66a184bb5a3cdef079172a4
Kevyn Nogueira <kevynnogueira@gmail.com>
2023-10-20 15:31:37 -0300

    Update app_mercado.py

--- app_mercado.py
+++ app_mercado.py
@@ -79,7 +79,7 @@ with st.expander("INFORMAÇÕES BÁSICAS"):
         st.write('[Dados de geolocalização do Open Adresses](https://openaddresses.io/)')
 
 
-st.cache_data
+@st.cache_data
 def carregar_dados():
     df = pd.read_parquet("df_completo.parquet")
     return df
